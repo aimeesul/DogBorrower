def16dca091a7374a1adc817a97793674053775d
Aimee Sullivan <a.sullivan.2025@gmail.com>
2022-04-05 16:37:30 +0100

    Create UI for login page

--- DogBorrower/ContentView.swift
+++ DogBorrower/ContentView.swift
@@ -6,13 +6,70 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello, aimee!")
-            .padding()
+        
+        VStack(
+            alignment: .center
+        ) {
+            
+            
+            
+            Image("logo")
+                .resizable()
+                .padding(.all)
+                .scaledToFit()
+            Form{
+                TextField(text: $username, prompt: Text("Username")) {
+                    
+                }
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                SecureField(text: $password, prompt: Text("Password")) {
+                    
+                }
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+                HStack{
+                    Spacer()
+                    Button(action: {
+                        print("sign up")
+                    }, label: {
+                        Text("Sign Up")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding(.all)
+                    })
+                    Spacer()
+                    Button(action: {
+                        print("log in")
+                    }, label: {
+                        Text("Log In")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding(.all)
+                    })
+                    Spacer()
+                }
+                .buttonStyle(.bordered)
+            }
+            
+        }
+        
+        
+        
+        
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
