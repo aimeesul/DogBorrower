5c3c3aa728cd1b3c9c1a165a551d9a0e18a2dd65
Aimee Sullivan <a.sullivan.2025@gmail.com>
2022-04-05 14:45:16 +0100

    Initial Commit

---
+++ DogBorrower/DogBorrowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBorrowerApp.swift
+//  DogBorrower
+//
+//  Created by Aimee Sullivan on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct DogBorrowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
